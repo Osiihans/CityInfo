beef0e3c669a3420794aa883e923bbc6c807b535
Osiihans <osiihans2@gmail.com>
2024-02-13 10:42:12 +0300

    Add project files.

---
+++ CityInfo.API/Entities/CastOperators.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class CastOperators
+    {
+        public int Id { get; set; }
+
+        public string ArtisteName { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
